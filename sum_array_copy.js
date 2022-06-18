b27507d474e4e4a7d102feb7586684083f86e072
iiiivanCh <iiiivanchm@yandex.ru>
2022-06-18 23:13:24 +0300

    aaaaaaaaaaaaaaaaaa

--- sum_array_copy.js
+++ sum_array_copy.js
@@ -1,19 +1,16 @@
 "use strict";
 
 const arrNew = [0, 5, 4];
-const newArr = Array.from(arrNew);
 
 const doSumArray = (arr) => {
+  const newArr = Array.from(arr);
+  const sum = newArr.reduce((acc, val) => acc + val, 0);
   const arrayMember = Math.trunc(Math.random() * 11);
-  arr.push(arrayMember);
-  const sum = arr.reduce((acc, val) => acc + val);
-  if (sum > 50) {
-    return arr;
-  }
-  doSumArray(arr);
+  newArr.push(arrayMember);
+  return sum > 50 ? newArr : doSumArray(newArr);
 };
 
-doSumArray(newArr);
+doSumArray(arrNew);
 
 console.log(arrNew);
-console.log(doSumArray(newArr));
+console.log(doSumArray(arrNew));
